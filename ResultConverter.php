dc1348ea494b6956e1ff387670dd2c3fcee0a6b8
Christopher Hertel <mail@christopher-hertel.de>
2025-07-18 11:15:56 +0200

    Rework Platform API to invoke and result

--- ResultConverter.php
+++ ResultConverter.php
@@ -13,32 +13,32 @@ namespace Symfony\AI\Platform\Bridge\Voyage;
 
 use Symfony\AI\Platform\Exception\RuntimeException;
 use Symfony\AI\Platform\Model;
-use Symfony\AI\Platform\Response\RawResponseInterface;
-use Symfony\AI\Platform\Response\ResponseInterface;
-use Symfony\AI\Platform\Response\VectorResponse;
-use Symfony\AI\Platform\ResponseConverterInterface;
+use Symfony\AI\Platform\Result\RawResultInterface;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\VectorResult;
+use Symfony\AI\Platform\ResultConverterInterface;
 use Symfony\AI\Platform\Vector\Vector;
 
 /**
  * @author Christopher Hertel <mail@christopher-hertel.de>
  */
-final readonly class ResponseConverter implements ResponseConverterInterface
+final readonly class ResultConverter implements ResultConverterInterface
 {
     public function supports(Model $model): bool
     {
         return $model instanceof Voyage;
     }
 
-    public function convert(RawResponseInterface $response, array $options = []): ResponseInterface
+    public function convert(RawResultInterface $result, array $options = []): ResultInterface
     {
-        $response = $response->getRawData();
+        $result = $result->getData();
 
-        if (!isset($response['data'])) {
+        if (!isset($result['data'])) {
             throw new RuntimeException('Response does not contain embedding data');
         }
 
-        $vectors = array_map(fn (array $data) => new Vector($data['embedding']), $response['data']);
+        $vectors = array_map(fn (array $data) => new Vector($data['embedding']), $result['data']);
 
-        return new VectorResponse($vectors[0]);
+        return new VectorResult($vectors[0]);
     }
 }
